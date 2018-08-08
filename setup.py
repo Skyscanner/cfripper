66a621ac1b265f12537f1c83224117bacdbd2a71
Valentin Tunev <valentin.tunev@skyscanner.net>
2018-08-08 15:21:58 +0300

    Fix typo in travis file

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ dev_requires = [
 
 setup(
     name='cfripper',
-    version='0.1.0',
+    version='0.1.1',
     author='Skyscanner Product Security',
     author_email='security@skyscanner.net',
     long_description=long_description,
