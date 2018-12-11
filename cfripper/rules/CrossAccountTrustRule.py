6fe02f220afe7b843305ea14315847146834021c
Valentin Tunev <valentin.tunev@skyscanner.net>
2018-12-11 10:13:51 +0200

    Fix self name bug

--- cfripper/rules/CrossAccountTrustRule.py
+++ cfripper/rules/CrossAccountTrustRule.py
@@ -40,7 +40,7 @@ class CrossAccountTrustRule(Rule):
 
             if not isinstance(principal, str):
                 logger.error(
-                    f"{self.__name__}/{self._config.stack_name}/{self._config.service_name} \
+                    f"{type(self).__name__}/{self._config.stack_name}/{self._config.service_name} \
                     - Skipping validation: principal is possibly a function."
                 )
                 continue
