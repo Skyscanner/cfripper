d6f30501653af7ab4ba15ef2b0cce118cff2ca40
Oscar Blanco <oscarbc1996@gmail.com>
2019-10-07 13:06:32 +0200

    reorganize tests

--- tests/rules/test_CloudFormationAuthenticationRule.py
+++ tests/rules/test_CloudFormationAuthenticationRule.py
@@ -12,30 +12,73 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
-
 import pytest
-import os
 import pycfmodel
 from cfripper.rules.CloudFormationAuthenticationRule import CloudFormationAuthenticationRule
-from cfripper.model.utils import convert_json_or_yaml_to_dict
 from cfripper.model.result import Result
 
 
 @pytest.fixture()
 def template_bad():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(f"{dir_path}/test_templates/cfn_authentication.json") as cf_script:
-        cf_template = convert_json_or_yaml_to_dict(cf_script.read())
-    return pycfmodel.parse(cf_template).resolve()
+    template = {
+        "Parameters": {"subnetId": {"Type": "String", "Default": "subnet-4fd01116"}},
+        "Resources": {
+            "EC2I4LBA1": {
+                "Type": "AWS::EC2::Instance",
+                "Properties": {"ImageId": "ami-6df1e514", "InstanceType": "t2.micro", "SubnetId": {"Ref": "subnetId"}},
+                "Metadata": {
+                    "AWS::CloudFormation::Authentication": {
+                        "testBasic": {
+                            "type": "basic",
+                            "username": "biff",
+                            "password": "badpassword",
+                            "uris": ["http://www.example.com/test"],
+                        }
+                    }
+                },
+            }
+        },
+    }
+    return pycfmodel.parse(template).resolve()
 
 
 @pytest.fixture()
 def template_good():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(f"{dir_path}/test_templates/cfn_authentication_good.json") as cf_script:
-        cf_template = convert_json_or_yaml_to_dict(cf_script.read())
-    return pycfmodel.parse(cf_template).resolve()
+    template = {
+        "Parameters": {
+            "subnetId": {"Type": "String", "Default": "subnet-4fd01116"},
+            "MasterUsername": {
+                "NoEcho": True,
+                "Description": "The database admin account name",
+                "MinLength": 8,
+                "Type": "String",
+            },
+            "MasterUserPassword": {
+                "NoEcho": True,
+                "Description": "The database admin account password",
+                "MinLength": 8,
+                "Type": "String",
+            },
+        },
+        "Resources": {
+            "EC2I4LBA1": {
+                "Type": "AWS::EC2::Instance",
+                "Properties": {"ImageId": "ami-6df1e514", "InstanceType": "t2.micro", "SubnetId": {"Ref": "subnetId"}},
+                "Metadata": {
+                    "AWS::CloudFormation::Authentication": {
+                        "testBasic": {
+                            "type": "basic",
+                            "username": {"Ref": "MasterUsername"},
+                            "password": {"Ref": "MasterUserPassword"},
+                            "uris": ["http://www.example.com/test"],
+                        }
+                    }
+                },
+            }
+        },
+    }
+
+    return pycfmodel.parse(template).resolve()
 
 
 def test_cfn_creds_found(template_bad):
