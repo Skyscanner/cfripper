b5772f3ad2ae028c4061c3d5a97ac3b6d4a56bb9
Oscar Blanco <oscarbc1996@gmail.com>
2019-10-09 17:10:38 +0200

    Update IAMRoleWildcardActionOnPermissionsPolicyRule

--- tests/rules/test_IAMRolesOverprivilegedRule.py
+++ tests/rules/test_IAMRolesOverprivilegedRule.py
@@ -70,6 +70,8 @@ def test_with_invalid_role_inline_policy(invalid_role_inline_policy):
     rule.invoke(invalid_role_inline_policy)
 
     assert not result.valid
+    assert len(result.failed_rules) == 1
+    assert len(result.failed_monitored_rules) == 0
     assert result.failed_rules[0]["rule"] == "IAMRolesOverprivilegedRule"
     assert (
         result.failed_rules[0]["reason"]
@@ -83,6 +85,8 @@ def test_with_invalid_role_inline_policy_resource_as_array(invalid_role_inline_p
     rule.invoke(invalid_role_inline_policy_resource_as_array)
 
     assert not result.valid
+    assert len(result.failed_rules) == 1
+    assert len(result.failed_monitored_rules) == 0
     assert result.failed_rules[0]["rule"] == "IAMRolesOverprivilegedRule"
     assert (
         result.failed_rules[0]["reason"]
@@ -106,6 +110,8 @@ def test_with_invalid_role_managed_policy(invalid_role_managed_policy):
     rule.invoke(invalid_role_managed_policy)
 
     assert not result.valid
+    assert len(result.failed_rules) == 1
+    assert len(result.failed_monitored_rules) == 0
     assert result.failed_rules[0]["rule"] == "IAMRolesOverprivilegedRule"
     assert (
         result.failed_rules[0]["reason"]
@@ -119,6 +125,8 @@ def test_with_invalid_role_inline_policy_fn_if(invalid_role_inline_policy_fn_if)
     rule.invoke(invalid_role_inline_policy_fn_if)
 
     assert not result.valid
+    assert len(result.failed_rules) == 1
+    assert len(result.failed_monitored_rules) == 0
     assert result.failed_rules[0]["rule"] == "IAMRolesOverprivilegedRule"
     assert (
         result.failed_rules[0]["reason"]
