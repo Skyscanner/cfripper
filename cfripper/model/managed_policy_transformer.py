2529d75b9227e9b0b86970adf944ac93c9759797
Oscar Blanco <oscarbc1996@gmail.com>
2019-10-09 16:46:47 +0200

    update managed policy transformes

--- cfripper/model/managed_policy_transformer.py
+++ cfripper/model/managed_policy_transformer.py
@@ -1,5 +1,5 @@
 """
-Copyright 2018 Skyscanner Ltd
+Copyright 2018-2019 Skyscanner Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 this file except in compliance with the License.
@@ -12,39 +12,28 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
-
 import boto3
 
 from pycfmodel.model.resources.properties.policy import Policy
 
 
-class ManagedPolicyTransformer(object):
-    """
-    Go through managed policie ARNs, fetch them and add them as
-    regular policies so that they can be checked by the rules.
-    """
-
-    def __init__(self, cf_model):
-        self.cf_model = cf_model
-        self.iam_client = boto3.client("iam")
-
-    def transform_managed_policies(self):
-        self.parse_fetch_update(self.cf_model.resources.get("AWS::IAM::Role", []))
-        self.parse_fetch_update(self.cf_model.resources.get("AWS::IAM::Group", []))
-
-    def parse_fetch_update(self, resources):
-        for resource in resources:
-            for managed_policy_arn in resource.managed_policy_arns:
-                managed_policy = self.iam_client.get_policy(PolicyArn=managed_policy_arn)
+def transform_managed_policies(cfmodel):
+    iam_client = boto3.client("iam")
+    for logical_id, resource in cfmodel.Resources.items():
+        if resource.Type in ["AWS::IAM::Role", "AWS::IAM::Group"] and resource.Properties.ManagedPolicyArns:
+            managed_policies = []
+            for managed_policy_arn in resource.Properties.ManagedPolicyArns:
+                managed_policy = iam_client.get_policy(PolicyArn=managed_policy_arn)
                 version_id = managed_policy.get("Policy", {}).get("DefaultVersionId")
-                if not version_id:
-                    continue
-
-                policy_version = self.iam_client.get_policy_version(PolicyArn=managed_policy_arn, VersionId=version_id)
-                policy_document_json = {
-                    "PolicyDocument": policy_version["PolicyVersion"]["Document"],
-                    "PolicyName": "AutoTransformedManagedPolicy{}".format(version_id),
-                }
-                policy_document = Policy(policy_document_json)
-                resource.policies.append(policy_document)
+                if version_id:
+                    policy_version = iam_client.get_policy_version(PolicyArn=managed_policy_arn, VersionId=version_id)
+                    managed_policies.append(
+                        Policy(
+                            **{
+                                "PolicyDocument": policy_version["PolicyVersion"]["Document"],
+                                "PolicyName": f"AutoTransformedManagedPolicy{version_id}",
+                            }
+                        )
+                    )
+            resource.Properties.ManagedPolicyArns = managed_policies
+    return cfmodel
