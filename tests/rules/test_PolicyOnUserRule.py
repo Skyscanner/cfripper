9051ec24c74befe20992c552da867b038d2a00f6
Jordi Soucheiron <jordi@soucheiron.cat>
2019-10-16 17:37:21 +0200

    Continue adding tests

--- tests/rules/test_PolicyOnUserRule.py
+++ tests/rules/test_PolicyOnUserRule.py
@@ -12,25 +12,44 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-# import pytest
-#
-# from cfripper.rules.PolicyOnUserRule import PolicyOnUserRule
-# from cfripper.model.result import Result
-# from tests.utils import get_cfmodel_from
+from pycfmodel.model.cf_model import CFModel
+from pytest import fixture
 
-# TODO Implement test
-# @pytest.fixture()
-# def abcdef():
-#     return get_cfmodel_from("rules/PolicyOnUserRule/abcdef.json").resolve()
-#
-#
-# def test_abcdef(abcdef):
-#     result = Result()
-#     rule = PolicyOnUserRule(None, result)
-#     rule.invoke(abcdef)
-#
-#     assert not result.valid
-#     assert len(result.failed_rules) == 1
-#     assert len(result.failed_monitored_rules) == 0
-#     assert result.failed_rules[0]["rule"] == "PolicyOnUserRule"
-#     assert result.failed_rules[0]["reason"] == "KMS Key policy {} should not allow wildcard principals"
+from cfripper.model.result import Result
+from cfripper.rules import PolicyOnUserRule
+from tests.utils import get_cfmodel_from
+
+
+@fixture()
+def good_template():
+    return get_cfmodel_from("rules/PolicyOnUserRule/good_template.json").resolve()
+
+
+@fixture()
+def bad_template():
+    return get_cfmodel_from("rules/PolicyOnUserRule/bad_template.json").resolve()
+
+
+def test_no_failures_are_raised(good_template):
+    result = Result()
+    rule = PolicyOnUserRule(None, result)
+    rule.invoke(good_template)
+
+    assert result.valid
+    assert len(result.failed_rules) == 0
+    assert len(result.failed_monitored_rules) == 0
+
+
+def test_failures_are_raised(bad_template: CFModel):
+    result = Result()
+    rule = PolicyOnUserRule(None, result)
+    rule.invoke(bad_template)
+
+    assert result.valid
+    assert len(result.failed_rules) == 0
+    assert len(result.failed_monitored_rules) == 1
+    assert result.failed_monitored_rules[0]["rule"] == "PolicyOnUserRule"
+    assert (
+        result.failed_monitored_rules[0]["reason"]
+        == "IAM policy Policy should not apply directly to users. Should be on group"
+    )
