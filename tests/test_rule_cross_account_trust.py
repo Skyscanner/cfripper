3125c1ec25daca18ef1d61e8ff16f7ccf2215be1
Nacho Rasche <nacho.rasche@skyscanner.net>
2019-10-16 16:13:51 +0200

    Make nice fixtures

--- tests/test_rule_cross_account_trust.py
+++ tests/test_rule_cross_account_trust.py
@@ -33,168 +33,19 @@ def template_one_role():
     return pycfmodel.parse(cf_template)
 
 
-def test_report_format_is_the_one_expected(template_one_role):
-    result = Result()
-    rule = CrossAccountTrustRule(Config(aws_account_id="123456789"), result)
-    rule.invoke(template_one_role.resources, template_one_role.parameters)
-
-    assert not result.valid
-    assert result.failed_rules == [
-        {
-            "rule": "CrossAccountTrustRule",
-            "reason": "RootRole has forbidden cross-account trust relationship with arn:aws:iam::123456789:root",
-            "rule_mode": RuleMode.BLOCKING,
-            "risk_value": RuleRisk.MEDIUM,
-            "resource_ids": {"RootRole"},
-            "actions": set(),
-            "granularity": RuleGranularity.RESOURCE,
-        },
-        {
-            "rule": "CrossAccountTrustRule",
-            "reason": "RootRole has forbidden cross-account trust relationship with arn:aws:iam::999999999:role/someuser@bla.com",  # noqa
-            "rule_mode": RuleMode.BLOCKING,
-            "risk_value": RuleRisk.MEDIUM,
-            "resource_ids": {"RootRole"},
-            "actions": set(),
-            "granularity": RuleGranularity.RESOURCE,
-        },
-    ]
+@pytest.fixture()
+def template_two_roles_dict():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(
+            f"{dir_path}/test_templates/iam_root_role_cross_account_two_roles.json"
+    ) as cf_script:
+        cf_template = convert_json_or_yaml_to_dict(cf_script.read())
+    return cf_template
 
 
-template_two_roles = {
-    "AWSTemplateFormatVersion": "2010-09-09",
-    "Resources": {
-        "RootRoleOne": {
-            "Type": "AWS::IAM::Role",
-            "Properties": {
-                "AssumeRolePolicyDocument": {
-                    "Version": "2012-10-17",
-                    "Statement": [
-                        {
-                            "Effect": "Allow",
-                            "Principal": {
-                                "AWS": [
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:root",
-                                    "arn:aws:iam::999999999:role/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                ]
-                            },
-                            "Action": "sts:AssumeRole",
-                        }
-                    ],
-                },
-                "Path": "/",
-                "Policies": [],
-            },
-        },
-        "RootRoleTwo": {
-            "Type": "AWS::IAM::Role",
-            "Properties": {
-                "AssumeRolePolicyDocument": {
-                    "Version": "2012-10-17",
-                    "Statement": [
-                        {
-                            "Effect": "Allow",
-                            "Principal": {
-                                "AWS": [
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:root",
-                                    "arn:aws:iam::999999999:role/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                    "arn:aws:iam::123456789:user/someuser@bla.com",
-                                ]
-                            },
-                            "Action": "sts:AssumeRole",
-                        }
-                    ],
-                },
-                "Path": "/",
-                "Policies": [],
-            },
-        },
-    },
-}
-
-
-def test_resource_whitelisting_works_as_expected():
-    result = Result()
-    mock_rule_to_resource_whitelist = {"CrossAccountTrustRule": {".*": {"RootRoleOne"}}}
-    mock_config = Config(
-        rules=["CrossAccountTrustRule"],
-        aws_account_id="123456789",
-        rule_to_resource_whitelist=mock_rule_to_resource_whitelist,
-        stack_name="mockstack",
-        stack_whitelist={},
-    )
-    rules = [DEFAULT_RULES.get(rule)(mock_config, result) for rule in mock_config.rules]
-    processor = RuleProcessor(*rules)
-    processor.process_cf_template(template_two_roles, mock_config, result)
-
-    assert not result.valid
-    assert result.failed_rules == [
-        {
-            "rule": "CrossAccountTrustRule",
-            "reason": "RootRoleTwo has forbidden cross-account trust relationship with arn:aws:iam::123456789:root",
-            "rule_mode": RuleMode.BLOCKING,
-            "risk_value": RuleRisk.MEDIUM,
-            "resource_ids": {"RootRoleTwo"},
-            "actions": set(),
-            "granularity": RuleGranularity.RESOURCE,
-        },
-        {
-            "rule": "CrossAccountTrustRule",
-            "reason": "RootRoleTwo has forbidden cross-account trust relationship with arn:aws:iam::999999999:role/someuser@bla.com",  # noqa
-            "rule_mode": RuleMode.BLOCKING,
-            "risk_value": RuleRisk.MEDIUM,
-            "resource_ids": {"RootRoleTwo"},
-            "actions": set(),
-            "granularity": RuleGranularity.RESOURCE,
-        },
-    ]
-
-
-def test_whitelisted_stacks_do_not_report_anything():
-    result = Result()
-    mock_stack_whitelist = {"mockstack": ["CrossAccountTrustRule"]}
-    mock_config = Config(
-        rules=["CrossAccountTrustRule"],
-        aws_account_id="123456789",
-        stack_name="mockstack",
-        stack_whitelist=mock_stack_whitelist,
-    )
-    rules = [DEFAULT_RULES.get(rule)(mock_config, result) for rule in mock_config.rules]
-    processor = RuleProcessor(*rules)
-    processor.process_cf_template(template_two_roles, mock_config, result)
-
-    assert result.valid
-
-
-def test_non_whitelisted_stacks_are_reported_normally():
-    result = Result()
-    mock_stack_whitelist = {"mockstack": ["CrossAccountTrustRule"]}
-    mock_config = Config(
-        rules=["CrossAccountTrustRule"],
-        aws_account_id="123456789",
-        stack_name="anotherstack",
-        stack_whitelist=mock_stack_whitelist,
-    )
-    rules = [DEFAULT_RULES.get(rule)(mock_config, result) for rule in mock_config.rules]
-    processor = RuleProcessor(*rules)
-    processor.process_cf_template(template_two_roles, mock_config, result)
-    assert not result.valid
-    assert result.failed_rules == [
+@pytest.fixture()
+def expected_result_two_roles():
+    yield [
         {
             "rule": "CrossAccountTrustRule",
             "reason": "RootRoleOne has forbidden cross-account trust relationship with arn:aws:iam::123456789:root",
@@ -232,3 +83,81 @@ def test_non_whitelisted_stacks_are_reported_normally():
             "granularity": RuleGranularity.RESOURCE,
         },
     ]
+
+
+def test_report_format_is_the_one_expected(template_one_role):
+    result = Result()
+    rule = CrossAccountTrustRule(Config(aws_account_id="123456789"), result)
+    rule.invoke(template_one_role.resources, template_one_role.parameters)
+
+    assert not result.valid
+    assert result.failed_rules == [
+        {
+            "rule": "CrossAccountTrustRule",
+            "reason": "RootRole has forbidden cross-account trust relationship with arn:aws:iam::123456789:root",
+            "rule_mode": RuleMode.BLOCKING,
+            "risk_value": RuleRisk.MEDIUM,
+            "resource_ids": {"RootRole"},
+            "actions": set(),
+            "granularity": RuleGranularity.RESOURCE,
+        },
+        {
+            "rule": "CrossAccountTrustRule",
+            "reason": "RootRole has forbidden cross-account trust relationship with arn:aws:iam::999999999:role/someuser@bla.com",  # noqa
+            "rule_mode": RuleMode.BLOCKING,
+            "risk_value": RuleRisk.MEDIUM,
+            "resource_ids": {"RootRole"},
+            "actions": set(),
+            "granularity": RuleGranularity.RESOURCE,
+        },
+    ]
+
+
+def test_resource_whitelisting_works_as_expected(template_two_roles_dict, expected_result_two_roles):
+    result = Result()
+    mock_rule_to_resource_whitelist = {"CrossAccountTrustRule": {".*": {"RootRoleOne"}}}
+    mock_config = Config(
+        rules=["CrossAccountTrustRule"],
+        aws_account_id="123456789",
+        rule_to_resource_whitelist=mock_rule_to_resource_whitelist,
+        stack_name="mockstack",
+        stack_whitelist={},
+    )
+    rules = [DEFAULT_RULES.get(rule)(mock_config, result) for rule in mock_config.rules]
+    processor = RuleProcessor(*rules)
+    processor.process_cf_template(template_two_roles_dict, mock_config, result)
+
+    assert not result.valid
+    assert result.failed_rules == expected_result_two_roles[-2:]
+
+
+def test_whitelisted_stacks_do_not_report_anything(template_two_roles_dict):
+    result = Result()
+    mock_stack_whitelist = {"mockstack": ["CrossAccountTrustRule"]}
+    mock_config = Config(
+        rules=["CrossAccountTrustRule"],
+        aws_account_id="123456789",
+        stack_name="mockstack",
+        stack_whitelist=mock_stack_whitelist,
+    )
+    rules = [DEFAULT_RULES.get(rule)(mock_config, result) for rule in mock_config.rules]
+    processor = RuleProcessor(*rules)
+    processor.process_cf_template(template_two_roles_dict, mock_config, result)
+
+    assert result.valid
+
+
+def test_non_whitelisted_stacks_are_reported_normally(template_two_roles_dict, expected_result_two_roles):
+    result = Result()
+    mock_stack_whitelist = {"mockstack": ["CrossAccountTrustRule"]}
+    mock_config = Config(
+        rules=["CrossAccountTrustRule"],
+        aws_account_id="123456789",
+        stack_name="anotherstack",
+        stack_whitelist=mock_stack_whitelist,
+    )
+    rules = [DEFAULT_RULES.get(rule)(mock_config, result) for rule in mock_config.rules]
+    processor = RuleProcessor(*rules)
+    processor.process_cf_template(template_two_roles_dict, mock_config, result)
+    assert not result.valid
+    assert result.failed_rules == expected_result_two_roles
