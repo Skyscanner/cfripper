e44e829b0e9ad0a3ee0b3fd918e75620ea0e8adf
Oscar Blanco <oscarbc1996@gmail.com>
2019-11-19 17:59:37 +0100

    fixes

--- cfripper/config/config.py
+++ cfripper/config/config.py
@@ -15,7 +15,7 @@ specific language governing permissions and limitations under the License.
 import re
 from typing import List
 
-from .whitelist import AWS_ELB_ACCOUNT_IDS, AWS_ELASTICACHE_CANONICAL_IDS
+from .whitelist import AWS_ELASTICACHE_CANONICAL_IDS, AWS_ELB_ACCOUNT_IDS
 from .whitelist import rule_to_action_whitelist as default_rule_to_action_whitelist
 from .whitelist import rule_to_resource_whitelist as default_rule_to_resource_whitelist
 from .whitelist import stack_whitelist as default_stack_whitelist
@@ -117,7 +117,7 @@ class Config:
             rule_to_resource_whitelist if rule_to_resource_whitelist is not None else default_rule_to_resource_whitelist
         )
         self.stack_whitelist = stack_whitelist if stack_whitelist is not None else default_stack_whitelist
-        if aws_service_accounts is not None:
+        if aws_service_accounts is None:
             self.aws_service_accounts = {
                 "elb": AWS_ELB_ACCOUNT_IDS,
                 "elasticache": AWS_ELASTICACHE_CANONICAL_IDS,
--- cfripper/model/principal_checking_rule.py
+++ cfripper/model/principal_checking_rule.py
@@ -12,6 +12,8 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from typing import List, Set
+
 from cfripper.model.rule import Rule
 
 
@@ -20,21 +22,21 @@ class PrincipalCheckingRule(Rule):
         super().__init__(*args, **kwargs)
         self._valid_principals = None
 
-    def get_whitelist_from_config(self, services=None):
+    def _get_whitelist_from_config(self, services: List[str] = None) -> Set[str]:
         if services is None:
             services = self._config.aws_service_accounts.keys()
 
         unique_list = set()
         for service in services:
-            unique_list.add(self._config.aws_service_accounts[service])
+            unique_list |= set(self._config.aws_service_accounts[service])
         return unique_list
 
     @property
-    def valid_principals(self):
+    def valid_principals(self) -> Set[str]:
         if self._valid_principals is None:
             self._valid_principals = {
                 *self._config.aws_principals,
-                self._config.aws_account_id,cfripper/model/principal_checking_rule.py
-                *self.get_whitelist_from_config()
+                self._config.aws_account_id,
+                *self._get_whitelist_from_config(),
             }
         return self._valid_principals
