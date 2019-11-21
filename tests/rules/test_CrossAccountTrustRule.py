97489b50ba1e0f97f885ae5c97d91779a244cdad
Oscar Blanco <oscarbc1996@gmail.com>
2019-11-21 12:08:29 +0100

    fix format

--- tests/rules/test_CrossAccountTrustRule.py
+++ tests/rules/test_CrossAccountTrustRule.py
@@ -39,6 +39,7 @@ def template_two_roles_dict():
 def template_valid_with_service():
     return get_cfmodel_from("rules/CrossAccountTrustRule/valid_with_service.json").resolve()
 
+
 @pytest.fixture()
 def template_valid_with_canonical_id():
     return get_cfmodel_from("rules/CrossAccountTrustRule/valid_with_canonical_id.json").resolve()
