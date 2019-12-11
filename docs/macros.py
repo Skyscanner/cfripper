46ebeee3d01ebbfcc51f06d48af4cc88c8ef6a81
Jordi Soucheiron <jordi@soucheiron.cat>
2019-12-11 16:12:44 +0100

    Remove unneded files

--- docs/macros.py
+++ docs/macros.py
@@ -3,21 +3,13 @@ from cfripper import rules
 
 
 def define_env(env):
-    """
-    This is the hook for defining variables, macros and filters
-
-    - variables: the dictionary that contains the environment variables
-    - macro: a decorator function, to declare a macro.
-    """
-
-    env.variables['baz'] = "John Doe"
-
     @env.macro
     def cfripper_rules():
         rules_inspection = inspect.getmembers(rules, inspect.isclass)
         results = []
         for _, klass in rules_inspection:
             doc = inspect.getdoc(klass)
+            # Remove ABCMeta default docstring
             if not doc.startswith('Helper class that'):
                 results.append((klass.__name__, doc))
             else:
