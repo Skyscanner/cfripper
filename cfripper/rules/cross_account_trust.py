b538400e8e1d3ca77c5644f46489d2894cce5c17
Jordi Soucheiron <jordi@soucheiron.cat>
2019-12-13 13:11:16 +0100

    Fix typo

--- cfripper/rules/cross_account_trust.py
+++ cfripper/rules/cross_account_trust.py
@@ -11,9 +11,9 @@ Unless required by applicable law or agreed to in writing, software distributed
 under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
-from cfripper.model.enums import RuleMode
 """
 __all__ = ["CrossAccountCheckingRule", "CrossAccountTrustRule", "S3CrossAccountTrustRule"]
+
 import logging
 import re
 from typing import Set
