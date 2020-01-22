a9add84186a13235add390f124cb1a5306b33cd7
Oscar Blanco <oscarbc1996@gmail.com>
2020-01-22 12:53:46 +0100

    add test

--- tests/model/test_utils.py
+++ tests/model/test_utils.py
@@ -14,7 +14,7 @@ specific language governing permissions and limitations under the License.
 """
 import pytest
 
-from cfripper.model.utils import extract_bucket_name_and_path_from_url
+from cfripper.model.utils import InvalidURLException, extract_bucket_name_and_path_from_url
 
 
 @pytest.mark.parametrize(
@@ -39,5 +39,20 @@ from cfripper.model.utils import extract_bucket_name_and_path_from_url
         ),
     ],
 )
-def test_stack_delete(template_url, bucket, path):
+def test_extract_bucket_name_and_path_from_url_works(template_url, bucket, path):
     assert extract_bucket_name_and_path_from_url(template_url) == (bucket, path)
+
+
+@pytest.mark.parametrize(
+    "template_url",
+    [
+        ("https://cf-templates.s3Xamazonaws.com/path/to/template.yml"),
+        ("https://cf-templates.s3-eu-central-1Xamazonaws.com/path/to/template.yml"),
+        ("https://s3Xamazonaws.com/cf-templates/path/to/template.yml"),
+        ("https://s3.eu-central-1XamazonawsXcom/cf-templates/path/to/template.yml"),
+        ("https://s3-eu-central-1XamazonawsXcom/cf-templates/path/to/template.yml"),
+    ],
+)
+def test_extract_bucket_name_and_path_from_url_fail(template_url):
+    with pytest.raises(InvalidURLException):
+        extract_bucket_name_and_path_from_url(template_url)
