7b2922fca752c484e1695999e41279d96f140c3e
Oscar Blanco <oscarbc1996@gmail.com>
2020-01-22 12:55:15 +0100

    add test

--- tests/model/test_utils.py
+++ tests/model/test_utils.py
@@ -46,10 +46,10 @@ def test_extract_bucket_name_and_path_from_url_works(template_url, bucket, path)
 @pytest.mark.parametrize(
     "template_url",
     [
-        ("https://cf-templates.s3Xamazonaws.com/path/to/template.yml"),
-        ("https://cf-templates.s3-eu-central-1Xamazonaws.com/path/to/template.yml"),
+        ("https://cf-templatesXs3Xamazonaws.com/path/to/template.yml"),
+        ("https://cf-templatesXs3-eu-central-1Xamazonaws.com/path/to/template.yml"),
         ("https://s3Xamazonaws.com/cf-templates/path/to/template.yml"),
-        ("https://s3.eu-central-1XamazonawsXcom/cf-templates/path/to/template.yml"),
+        ("https://s3Xeu-central-1XamazonawsXcom/cf-templates/path/to/template.yml"),
         ("https://s3-eu-central-1XamazonawsXcom/cf-templates/path/to/template.yml"),
     ],
 )
