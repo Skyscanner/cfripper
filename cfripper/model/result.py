412bd3eb6fd9debaacc41af63315b21322e66d33
Oscar Blanco <oscarbc1996@gmail.com>
2020-05-21 10:00:37 +0200

    add  type checking if

--- cfripper/model/result.py
+++ cfripper/model/result.py
@@ -1,9 +1,11 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
 from pydantic import BaseModel, Extra
-from pydantic.typing import AbstractSetIntStr, DictIntStrAny
 
 from cfripper.model.enums import RuleMode
+
+if TYPE_CHECKING:
+    from pydantic.typing import AbstractSetIntStr, DictIntStrAny
 
 
 class Failure(BaseModel):
@@ -47,8 +49,8 @@ class Result(BaseModel):
     def dict(
         self,
         *,
-        include: Union[AbstractSetIntStr, DictIntStrAny] = None,
-        exclude: Union[AbstractSetIntStr, DictIntStrAny] = None,
+        include: Union["AbstractSetIntStr", "DictIntStrAny"] = None,
+        exclude: Union["AbstractSetIntStr", "DictIntStrAny"] = None,
         by_alias: bool = False,
         skip_defaults: bool = None,
         exclude_unset: bool = False,
