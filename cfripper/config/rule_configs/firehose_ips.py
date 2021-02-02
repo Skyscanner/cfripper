b33bfd8de9cfbcc1eb6ceeab4fbe5b1d5e6c36c4
Oliver Crawford <oliver.crawford@skyscanner.net>
2021-02-02 14:43:28 +0000

    Turn list to set and add comments on region in which IP is for

--- cfripper/config/rule_configs/firehose_ips.py
+++ cfripper/config/rule_configs/firehose_ips.py
@@ -19,32 +19,32 @@ config = Config(
 """
 
 # Adapted from https://docs.aws.amazon.com/firehose/latest/dev/controlling-access.html
-FIREHOSE_IPS = [
-    "13.58.135.96/27",
-    "52.70.63.192/27",
-    "13.57.135.192/27",
-    "52.89.255.224/27",
-    "18.253.138.96/27",
-    "52.61.204.160/27",
-    "35.183.92.128/27",
-    "18.162.221.32/27",
-    "13.232.67.32/27",
-    "13.209.1.64/27",
-    "13.228.64.192/27",
-    "13.210.67.224/27",
-    "13.113.196.224/27",
-    "52.81.151.32/27",
-    "161.189.23.64/27",
-    "35.158.127.160/27",
-    "52.19.239.192/27",
-    "18.130.1.96/27",
-    "35.180.1.96/27",
-    "13.53.63.224/27",
-    "15.185.91.0/27",
-    "18.228.1.128/27",
-    "15.161.135.128/27",
-    "13.244.121.224/27",
-]
+FIREHOSE_IPS = {
+    "13.113.196.224/27",  # Asia Pacific (Tokyo)
+    "13.209.1.64/27",  # Asia Pacific (Seoul)
+    "13.210.67.224/27",  # Asia Pacific (Sydney)
+    "13.228.64.192/27",  # Asia Pacific (Singapore)
+    "13.232.67.32/27",  # Asia Pacific (Mumbai)
+    "13.244.121.224/277",  # Africa (Cape Town)
+    "13.53.63.224/27",  # Europe (Stockholm)
+    "13.57.135.192/27",  # US West (N. California)
+    "13.58.135.96/27",  # US East (Ohio)
+    "15.161.135.128/27",  # Europe (Milan)
+    "15.185.91.0/27",  # Middle East (Bahrain)
+    "161.189.23.64/27",  # China (Ningxia)
+    "18.130.1.96/27",  # Europe (London)
+    "18.162.221.32/27",  # Asia Pacific (Hong Kong)
+    "18.228.1.128/27",  # South America (São Paulo)
+    "18.253.138.96/27",  # AWS GovCloud (US-East)
+    "35.158.127.160/27",  # Europe (Frankfurt)
+    "35.180.1.96/27",  # Europe (Paris)
+    "35.183.92.128/27",  # Canada (Central)
+    "52.19.239.192/27",  # Europe (Ireland)
+    "52.61.204.160/27",  # AWS GovCloud (US-West)
+    "52.70.63.192/27",  # US East (N. Virginia)
+    "52.81.151.32/27",  # China (Beijing)
+    "52.89.255.224/27",  # US West (Oregon)
+}
 
 firehose_ips_rules_config_filter = Filter(
     reason=(
