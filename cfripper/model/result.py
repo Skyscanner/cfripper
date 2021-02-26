250737443e49393d8f4e91105e61d3afe2285bbd
Oscar Blanco <oscarbc1996@gmail.com>
2021-02-26 11:51:58 +0100

    refactor result and failure class

--- cfripper/model/result.py
+++ cfripper/model/result.py
@@ -1,16 +1,16 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import List, Optional, Set
 
 from pydantic import BaseModel, Extra
 
-from cfripper.model.enums import RuleMode
+from cfripper.model.enums import RuleGranularity, RuleMode, RuleRisk
 
 
 class Failure(BaseModel):
-    granularity: str
+    granularity: RuleGranularity
     reason: str
-    risk_value: str
+    risk_value: RuleRisk
     rule: str
-    rule_mode: str
+    rule_mode: RuleMode
     actions: Optional[set] = set()
     resource_ids: Optional[set] = set()
 
@@ -32,105 +32,51 @@ class Failure(BaseModel):
 class Result(BaseModel):
     class Config(BaseModel.Config):
         extra = Extra.forbid
+        arbitrary_types_allowed = True
 
-    failed_rules: List[Failure] = []
-    exceptions: List = []
-    failed_monitored_rules: List[Failure] = []
-    warnings: List[Failure] = []
+    exceptions: List[Exception] = []
+    failures: List[Failure] = []
 
-    # Temporary fix until https://github.com/samuelcolvin/pydantic/issues/935 is fixed
-    @classmethod
-    def get_properties(cls):
-        return [prop for prop in cls.__dict__ if isinstance(cls.__dict__[prop], property)]
+    def get_failures(self, include_rule_modes: Set[RuleMode] = set(), exclude_rule_modes: Set[RuleMode] = set()):
+        result = []
+        for failure in self.failures:
+            if (not exclude_rule_modes or failure.rule_mode not in exclude_rule_modes) and (
+                not include_rule_modes or failure.rule_mode in include_rule_modes
+            ):
+                result.append(failure)
+        return result
 
-    def dict(
-        self,
-        *,
-        include: Union["AbstractSetIntStr", "DictIntStrAny"] = None,  # noqa: F821
-        exclude: Union["AbstractSetIntStr", "DictIntStrAny"] = None,  # noqa: F821
-        by_alias: bool = False,
-        skip_defaults: bool = None,
-        exclude_unset: bool = False,
-        exclude_defaults: bool = False,
-        exclude_none: bool = False,
-    ) -> Dict[str, Any]:
-        """Override the dict function to include our properties"""
-        attribs = super().dict(
-            include=include,
-            exclude=exclude,
-            by_alias=by_alias,
-            skip_defaults=skip_defaults,
-            exclude_unset=exclude_unset,
-            exclude_defaults=exclude_defaults,
-            exclude_none=exclude_none,
-        )
-        props = self.get_properties()
-
-        # Include and exclude properties
-        if include:
-            props = [prop for prop in props if prop in include]
-        if exclude:
-            props = [prop for prop in props if prop not in exclude]
-
-        # Update the attribute dict with the properties
-        if props:
-            attribs.update({prop: getattr(self, prop) for prop in props})
-        return attribs
-
-    def __repr_args__(self):
-        return self.dict().items()  # type: ignore
-
-    # End of temporary fix
-
-    def add_failure(
-        self, rule: str, reason: str, rule_mode: str, risk_value: str, granularity: str, resource_ids=None, actions=None
-    ):
-
-        if resource_ids is None:
-            resource_ids = set()
-
-        if actions is None:
-            actions = set()
-
-        failure = Failure(
-            rule=rule,
-            reason=reason,
-            rule_mode=rule_mode,
-            risk_value=risk_value,
-            resource_ids=resource_ids,
-            actions=actions,
-            granularity=granularity,
-        )
-
-        if rule_mode is not RuleMode.BLOCKING:
-            self.add_failure_monitored_rule(failure=failure)
-            return
-
-        self.add_failure_blocking_rule(failure=failure)
-
-    def add_exception(self, ex):
+    def add_exception(self, ex: Exception):
         self.exceptions.append(ex)
 
-    def add_warning(self, warning: Failure):
-        self.warnings.append(warning)
-
-    def add_failure_monitored_rule(self, failure: Failure):
-        self.failed_monitored_rules.append(failure)
-
-    def add_failure_blocking_rule(self, failure: Failure):
-        self.failed_rules.append(failure)
+    def add_failure(
+        self,
+        rule: str,
+        reason: str,
+        rule_mode: RuleMode,
+        risk_value: RuleRisk,
+        granularity: RuleGranularity,
+        resource_ids=None,
+        actions=None,
+    ):
+        self.failures.append(
+            Failure(
+                rule=rule,
+                reason=reason,
+                rule_mode=rule_mode,
+                risk_value=risk_value,
+                resource_ids=resource_ids,
+                actions=actions,
+                granularity=granularity,
+            )
+        )
 
     @property
     def valid(self) -> bool:
-        return not bool([rule for rule in self.failed_rules if rule.rule_mode == RuleMode.BLOCKING])
+        return not any(failure for failure in self.failures if failure.rule_mode == RuleMode.BLOCKING)
 
     def __add__(self, other):
         if not isinstance(other, self.__class__):
             return NotImplemented
 
-        return Result(
-            failed_rules=self.failed_rules + other.failed_rules,
-            exceptions=self.exceptions + other.exceptions,
-            failed_monitored_rules=self.failed_monitored_rules + other.failed_monitored_rules,
-            warnings=self.warnings + other.warnings,
-        )
+        return Result(exceptions=self.exceptions + other.exceptions, failures=self.failures + other.failures)
