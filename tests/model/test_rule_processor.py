40e77603298fefed5587ce34aa9cb87440863e9a
Oscar Blanco <oscarbc1996@gmail.com>
2021-02-26 10:44:55 +0100

    Feature: don't run disabled rules (#157)
    * dont run disabled rules
    
    * add tests

--- tests/model/test_rule_processor.py
+++ tests/model/test_rule_processor.py
@@ -58,3 +58,29 @@ def test_remove_debug_rules():
 def test_remove_debug_rules_no_rules():
     processed_list = RuleProcessor.remove_debug_rules(rules=[])
     assert [] == processed_list
+
+
+def test_rule_processor_invoke_rules():
+    rule = Mock()
+    rule.rule_mode = RuleMode.BLOCKING
+
+    processor = RuleProcessor(rule)
+
+    template = Mock()
+    config = Mock()
+    processor.process_cf_template(template, config)
+
+    rule.invoke.assert_called()
+
+
+def test_rule_processor_dont_invoke_disabled_rules():
+    rule = Mock()
+    rule.rule_mode = RuleMode.DISABLED
+
+    processor = RuleProcessor(rule)
+
+    template = Mock()
+    config = Mock()
+    processor.process_cf_template(template, config)
+
+    rule.invoke.assert_not_called()
