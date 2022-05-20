41a948bbf9fb6e497e5479b177884b692fedd99b
Ramon <ramon.guimera@skyscanner.net>
2022-05-20 09:03:46 +0200

    fix lint

--- tests/rules/test_RDSSecurityGroupIngressOpenToWorldRule.py
+++ tests/rules/test_RDSSecurityGroupIngressOpenToWorldRule.py
@@ -28,8 +28,7 @@ def test_dangerous_rds_securitygroup(rds_sg):
                 actions=None,
                 granularity=RuleGranularity.RESOURCE,
                 reason=(
-                    "RDS DB Security group NonCompliantRDSSecurityGroup "
-                    + "should not have ingress open to the world"
+                    "RDS DB Security group NonCompliantRDSSecurityGroup " + "should not have ingress open to the world"
                 ),
                 risk_value=RuleRisk.HIGH,
                 rule="RDSSecurityGroupIngressOpenToWorldRule",
