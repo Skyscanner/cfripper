6849776ad7503b118fd841186a78e2c196386125
Chrisando Ryan <chrisando.pardomuan@skyscanner.net>
2026-01-22 07:49:29 +0000

    Fix formatting

--- cfripper/rules/slingshot_secret_noecho.py
+++ cfripper/rules/slingshot_secret_noecho.py
@@ -1,9 +1,9 @@
 __all__ = ["SlingshotSecretNoEchoRule"]
 
-from pycfmodel.model.parameter import Parameter
 from typing import Any, Dict, Optional
 
 from pycfmodel.model.cf_model import CFModel
+from pycfmodel.model.parameter import Parameter
 
 from cfripper.model.enums import RuleGranularity
 from cfripper.model.result import Result
